b24eb4e3430c10483b02eb9bb0028c73b042f6da
nadiazafirazahra <114532973+nadiazafirazahra@users.noreply.github.com>
2022-10-27 15:24:18 +0700

    Add files via upload

---
+++ Anak.java
@@ -0,0 +1,16 @@
+/**
+ * Praktikum-Inheritance_Pemprograman Berorientasi Objek
+ * Nama: Nadia Zafira Zahra
+ * NIM: 20210040174
+ * Kelas: TI21F
+ * @author ACER
+ */
+
+public class Anak extends Bapak{
+    int c;
+public void show_Variabel(){
+    System.out.println("NIlai a="+ super.a);
+    System.out.println("NIlai b="+ super.b);
+    System.out.println("NIlai c="+ c);
+}
+}
