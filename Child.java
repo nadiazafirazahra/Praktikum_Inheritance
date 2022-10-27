7348082204d7ecd2a5142bbb9b651caefc09d0f9
nadiazafirazahra <114532973+nadiazafirazahra@users.noreply.github.com>
2022-10-27 15:36:45 +0700

    Add files via upload

---
+++ Child.java
@@ -0,0 +1,18 @@
+/**
+ * Praktikum-Inheritance_Pemprograman Berorientasi Objek
+ * Nama: Nadia Zafira Zahra
+ * NIM: 20210040174
+ * Kelas: TI21F
+ * @author ACER
+ */
+
+public class Parent {
+    
+public class Child extends Parent{
+     int x;
+    public Child() {
+        x = 5;
+    }
+}
+
+}
