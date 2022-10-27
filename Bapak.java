757c011447b227b679503a0de52de7f4d75d28bf
nadiazafirazahra <114532973+nadiazafirazahra@users.noreply.github.com>
2022-10-27 15:27:08 +0700

    Add files via upload

---
+++ Bapak.java
@@ -0,0 +1,17 @@
+/**
+ * Praktikum-Inheritance_Pemprograman Berorientasi Objek
+ * Nama: Nadia Zafira Zahra
+ * NIM: 20210040174
+ * Kelas: TI21F
+ * @author ACER
+ */
+
+public class Bapak {
+    int a;
+    int b;
+    
+public void show_variabel(){
+System.out.println("NIlai a="+ a);
+System.out.println("NIlai b="+ b);
+}
+}
